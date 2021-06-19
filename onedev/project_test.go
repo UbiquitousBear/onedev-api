768d675c9001a019c5aadd7974bbbbb4df18eccf
Shamil Nunhuck <shamil@shamil.co.uk>
2021-06-19 16:59:31 +0100

    Added a test for Reading a project

--- onedev/project_test.go
+++ onedev/project_test.go
@@ -1,6 +1,7 @@
 package onedev
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -11,6 +12,7 @@ import (
 )
 
 func TestProjectService_List(t *testing.T) {
+	// Arrange
 	client, mux, _, teardown := setup()
 	defer teardown()
 
@@ -19,15 +21,18 @@ func TestProjectService_List(t *testing.T) {
 		fmt.Fprint(w, `[{"id":1},{"id":2}]`)
 	})
 
+	// Act
 	ctx := context.Background()
 	got, _, err := client.Projects.List(ctx, 0, 100)
-	assert.NoError(t, err)
 
+	// Assert
+	assert.NoError(t, err)
 	want := []Project{{Id: Int(1)}, {Id: Int(2)}}
 	assert.Equal(t, &want, got)
 }
 
 func TestProjectService_Create(t *testing.T) {
+	// Arrange
 	client, mux, _, teardown := setup()
 	defer teardown()
 
@@ -37,15 +42,43 @@ func TestProjectService_Create(t *testing.T) {
 		json.NewDecoder(r.Body).Decode(v)
 		fmt.Fprint(w, `2`)
 	})
-
-	ctx := context.Background()
-
 	input := &Project{Name: String("foo"), Description: String("bar")}
+
+	// Act
+	ctx := context.Background()
 	got, _, err := client.Projects.Create(ctx, input)
+
+	// Assert
 	if err != nil {
 		t.Errorf("Projects.Create returned error: %v", err)
 	}
 
 	want := Project{Id: Int(2), Name: String("foo"), Description: String("bar")}
+	assert.Equal(t, want, *got)
+}
+
+func TestProjectService_Read(t *testing.T) {
+	// Arrange
+	client, mux, _, teardown := setup()
+	defer teardown()
+	id := 2
+	want := Project{Id: Int(id), Name: String("foo"), Description: String("bar")}
+
+	mux.HandleFunc("/projects/", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		buf := new(bytes.Buffer)
+		json.NewEncoder(buf).Encode(want)
+		fmt.Fprint(w, buf.String())
+	})
+
+
+	ctx := context.Background()
+	got, _, err := client.Projects.Read(ctx, id)
+
+	// Assert
+	if err != nil {
+		t.Errorf("Projects.Read returned error: %v", err)
+	}
+
 	assert.Equal(t, want, *got)
 }
